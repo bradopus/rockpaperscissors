34bffd0d793e7e65907873fdfa7a4e8e3a0a0d9a
bradopus <96777435+bradopus@users.noreply.github.com>
2023-06-27 01:04:07 -0400

    Adds basic functionality of rock, paper, scissors to the console

---
+++ rps.js
@@ -0,0 +1,95 @@
+let computerScore = 0;
+let playerScore = 0;
+
+const choices = ["rock","paper","scissors"];
+
+/* Randomly returns Rock, Paper, or Scissors */
+function getComputerChoice() {
+
+    let result = Math.floor(Math.random() * 3);
+    return choices[result]; // Returns txt value of choices array
+}
+
+/* Plays round, returns winner
+Ex: "You Lose! Paper beats Rock" */
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock, paper, or scissors?");
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return console.log("You both chose rock!  Tie!");
+                break;
+            case "paper":
+                computerScore++;
+                return console.log("Computer wins!  Paper beats rock.");
+                break;
+            case "scissors":
+                playerScore++;
+                return console.log("You win!  Rock beats scissors!");
+                break;
+        }
+    }
+    else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                playerScore++
+                return console.log("You win!  Paper beats rock.");
+                break;
+            case "paper":
+                return console.log("You both chose paper!  Tie!");
+                break;
+            case "scissors":
+                computerScore++;
+                return console.log("Computer wins!  Scissors beats paper!");
+                break;
+        }
+    }
+    else if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "rock":
+                computerScore++;
+                return console.log("Computer wins!  Rock beats scissors!");
+                break;
+            case "paper":
+                playerScore++;
+                return console.log("You win!  Scissors beats paper!");
+                break;
+            case "scissors":
+                return console.log("You both chose scissors!  Tie!");
+                break;
+        }
+    }
+    else {
+        console.log("Invalid answer, please try again.")
+    }
+
+}
+
+/* Plays 5 rounds, keeps score, declares results */
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+
+    console.log("Player Score: " + playerScore + "  Computer Score: " + computerScore);
+
+    if (playerScore > computerScore) {
+        console.log("Player wins!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("Computer wins!");
+    }
+    else {
+        console.log("It's a tie!");
+    }
+
+    playerScore = 0;
+    computerScore = 0;
+}
